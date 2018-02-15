85744bf38e26e19e4c3991ac4592faf13adebe78
Dmitriy Tolstikov <x79183603279@gmail.com>
2018-02-15 21:45:20 +0300

    8bit music, KSA, LHS

---
+++ zxmusic.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+for (( ; ; ))
+do
+	# LHS
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/lhs/lhs_myth1.xm"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/lhs/lhs_rld3.xm"
+
+    # KSA
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/Influence #3.stp"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/italian disco.pt1"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/JAM!-_5.stc"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/KOLOBOK.stc"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/KORA-RESONAN7.stp"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/KORA-YO-MOYO.stp"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/KORNELIUS.stc"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/KOTO.stc"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/KSA 1.pt2"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/KSA 11.stc"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/KSA 12.stp"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/KSA 14.stc"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/KSA 17-ICECLIM6.stc"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/KSA 22.stp"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/KSA 23.ay"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/KSA 3.pt2"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/KSA 4.pt2"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/KSA 5.pt2"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/KSA 6.pt2"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/ksa - double 2.stc"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/KSA-GLUK 1.stc"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/KSA GLUK 2.stc"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/KSAGLUK3.stc"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/KSA INS.stp"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/KSA INS (Party Version).stp"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/KSA-MTV.stc"
+    zxtune123 --alsa = "/home/dima/ya/Музыка/8bit/ksa/KSAMUSIC.pt3"
+done
